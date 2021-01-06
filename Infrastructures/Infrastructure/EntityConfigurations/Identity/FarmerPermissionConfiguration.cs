40f5ad5afd220ba8ee11077bfff5e72f66ab2fb2
HO HUYNH THANH NAM <nam.t171846@vanlanguni.vn>
2021-01-06 14:24:45 +0700

    add farmer permission

---
+++ Infrastructures/Infrastructure/EntityConfigurations/Identity/FarmerPermissionConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using ApplicationDomain.Identity.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.EntityConfigurations.Identity
+{
+    public class FarmerPermissionConfiguration : EntityConfigurationBase<FarmerPermission, int>
+    {
+        public override void OnConfigure(EntityTypeBuilder<FarmerPermission> builder)
+        {
+            builder.Property(p => p.CreatedDate).HasDefaultValue(DateTimeOffset.Now);
+            builder.Property(p => p.UpdatedDate).HasDefaultValue(DateTimeOffset.Now);
+            builder.HasOne(p => p.PermissionGroup).WithMany().OnDelete(Microsoft.EntityFrameworkCore.DeleteBehavior.Restrict);
+        }
+
+    }
+}
