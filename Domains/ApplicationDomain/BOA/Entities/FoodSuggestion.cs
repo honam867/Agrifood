9a31471d31f06004319439a5806bbf87fd89f076
Administrator <Administrator@DESKTOP-O5OU3KI>
2021-01-08 02:47:16 -0800

    Entity Cow And FoodSugestion

---
+++ Domains/ApplicationDomain/BOA/Entities/FoodSuggestion.cs
@@ -0,0 +1,11 @@
+﻿using ApplicationDomain.Core.Entities;
+
+namespace ApplicationDomain.BOA.Entities
+{
+    public class FoodSuggestion : EntityBase<int>
+    {
+        public int CowId { set; get; }
+        public Cow Cow { set; get; }
+        public string Content { set; get; }
+    }
+}
