0bb8bb8188d5a0d3f50440896cb6cef2e63c68ea
X161026 - NGUYEN THANH NAM - K22X01 <x161026@vanlanguni.vn>
2021-03-11 13:53:23 +0700

    List byre & unit test farmer list

---
+++ Applications/WebAdminApplication/ClientApp/src/app/modules/farmer/models/byre.ts
@@ -0,0 +1,19 @@
+export class Byre {
+  id: number;
+  name: string;
+  code: string;
+  breedId: number;
+  quantityCow:number;
+  farmerId: number;
+  ration: string;
+
+  constructor() {
+   this.id =0;
+   this.name="";
+   this.code="";
+   this.breedId=0;
+   this.quantityCow=0;
+   this.farmerId=0;
+   this.ration="";
+  }
+}
