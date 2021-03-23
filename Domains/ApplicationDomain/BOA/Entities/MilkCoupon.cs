74639ad06e28749d282a4d0797035c68acee6902
honam867 <57583732+honam867@users.noreply.github.com>
2021-03-23 20:15:35 +0700

    milk coupon. milk coupon detail

---
+++ Domains/ApplicationDomain/BOA/Entities/MilkCoupon.cs
@@ -0,0 +1,24 @@
+﻿using ApplicationDomain.Core.Entities;
+using ApplicationDomain.Identity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationDomain.BOA.Entities
+{
+    public class MilkCoupon : EntityBase<int>
+    {
+        public string Code { get; set; }
+        public int FarmerId { get; set; }
+        public Farmer Farmer { get; set; }
+        public string ScaleCode { get; set; }
+        public int EmployeeId { get; set; }
+        public Employee Employee { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string StorageTankCode { get; set; }
+        public string Session { get; set; }
+        public int MilkCollectionStationId { get; set; }
+        public MilkCollectionStation MilkCollectionStation { get; set; }
+
+    }
+}
--- Infrastructures/Infrastructure.DbMigration/Migrations/20210323131506_MilkCouponDetail.Designer.cs
+++ Infrastructures/Infrastructure.DbMigration/Migrations/20210323131506_MilkCouponDetail.Designer.cs
@@ -4,14 +4,16 @@ using Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Infrastructure.DbMigration.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210323131506_MilkCouponDetail")]
+    partial class MilkCouponDetail
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
