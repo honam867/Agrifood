9159fbfeb0d1794ef7362556c05b86579847aa6a
JayThien <80061797+JayThien@users.noreply.github.com>
2021-04-10 19:20:34 +0700

    Create TypeOfMilk API

---
+++ Domains/ApplicationDomain/BOA/IRepositories/ITypeOfMilkRepository.cs
@@ -0,0 +1,17 @@
+﻿using ApplicationDomain.BOA.Entities;
+using AspNetCore.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationDomain.BOA.IRepositories
+{
+    public interface ITypeOfMilkRepository : IGenericRepository<TypeOfMilk, int>
+    {
+        IQueryable GetTypeOfMilks();
+        IQueryable GetTypeOfMilkById(int id);
+        Task<bool> CheckCodeExistsAsync(string code);
+    }
+}
---
+++ Domains/ApplicationDomain/BOA/Models/TypeOfMilks/TypeOfMilkModel.cs
@@ -0,0 +1,22 @@
+﻿using ApplicationDomain.BOA.Entities;
+using AutoMapper;
+
+
+namespace ApplicationDomain.BOA.Models.TypeOfMilks
+{
+    public class TypeOfMilkModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } //NOT NULL, LENGTH > 5, UPPER CASE
+        public string Code { set; get; } //NOT NULL, LENGTH = 3, UPPER CASE, UNIQUE
+       
+    }
+    public class TypeOfMilkModelMapper : Profile
+    {
+        public TypeOfMilkModelMapper()
+        {
+            CreateMap<TypeOfMilkModel, TypeOfMilk>();
+            var mapers = CreateMap<TypeOfMilk, TypeOfMilkModel>();
+        }
+    }
+}
