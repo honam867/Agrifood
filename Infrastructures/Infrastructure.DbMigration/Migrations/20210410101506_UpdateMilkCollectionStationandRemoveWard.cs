219706fb81b95a9a82c4144d6ad53617cab37a7b
JayThien <80061797+JayThien@users.noreply.github.com>
2021-04-10 17:34:20 +0700

    CreateTypeOfMilkEntity

---
+++ Infrastructures/Infrastructure.DbMigration/Migrations/20210410101506_UpdateMilkCollectionStationandRemoveWard.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.DbMigration.Migrations
+{
+    public partial class UpdateMilkCollectionStationandRemoveWard : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MilkCollectionStation_Ward_WardId",
+                table: "MilkCollectionStation");
+
+            migrationBuilder.DropTable(
+                name: "Ward");
+
+            migrationBuilder.RenameColumn(
+                name: "WardId",
+                table: "MilkCollectionStation",
+                newName: "DistrictId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_MilkCollectionStation_WardId",
+                table: "MilkCollectionStation",
+                newName: "IX_MilkCollectionStation_DistrictId");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "UpdatedDate",
+                table: "PermissionMembership",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(2021, 4, 10, 17, 15, 6, 72, DateTimeKind.Unspecified).AddTicks(3584), new TimeSpan(0, 7, 0, 0, 0)),
+                oldClrType: typeof(DateTimeOffset),
+                oldDefaultValue: new DateTimeOffset(new DateTime(2021, 4, 6, 20, 10, 28, 303, DateTimeKind.Unspecified).AddTicks(2110), new TimeSpan(0, 7, 0, 0, 0)));
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "CreatedDate",
+                table: "PermissionMembership",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(2021, 4, 10, 17, 15, 6, 72, DateTimeKind.Unspecified).AddTicks(3241), new TimeSpan(0, 7, 0, 0, 0)),
+                oldClrType: typeof(DateTimeOffset),
+                oldDefaultValue: new DateTimeOffset(new DateTime(2021, 4, 6, 20, 10, 28, 303, DateTimeKind.Unspecified).AddTicks(1637), new TimeSpan(0, 7, 0, 0, 0)));
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "UpdatedDate",
+                table: "PermissionGroup",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(2021, 4, 10, 17, 15, 6, 69, DateTimeKind.Unspecified).AddTicks(211), new TimeSpan(0, 7, 0, 0, 0)),
+                oldClrType: typeof(DateTimeOffset),
+                oldDefaultValue: new DateTimeOffset(new DateTime(2021, 4, 6, 20, 10, 28, 299, DateTimeKind.Unspecified).AddTicks(563), new TimeSpan(0, 7, 0, 0, 0)));
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "CreatedDate",
+                table: "PermissionGroup",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(2021, 4, 10, 17, 15, 6, 68, DateTimeKind.Unspecified).AddTicks(9874), new TimeSpan(0, 7, 0, 0, 0)),
+                oldClrType: typeof(DateTimeOffset),
+                oldDefaultValue: new DateTimeOffset(new DateTime(2021, 4, 6, 20, 10, 28, 299, DateTimeKind.Unspecified).AddTicks(121), new TimeSpan(0, 7, 0, 0, 0)));
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "UpdatedDate",
+                table: "MenuPermission",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(2021, 4, 10, 17, 15, 6, 67, DateTimeKind.Unspecified).AddTicks(7249), new TimeSpan(0, 7, 0, 0, 0)),
+                oldClrType: typeof(DateTimeOffset),
+                oldDefaultValue: new DateTimeOffset(new DateTime(2021, 4, 6, 20, 10, 28, 298, DateTimeKind.Unspecified).AddTicks(853), new TimeSpan(0, 7, 0, 0, 0)));
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "CreatedDate",
+                table: "MenuPermission",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(2021, 4, 10, 17, 15, 6, 67, DateTimeKind.Unspecified).AddTicks(6882), new TimeSpan(0, 7, 0, 0, 0)),
+                oldClrType: typeof(DateTimeOffset),
+                oldDefaultValue: new DateTimeOffset(new DateTime(2021, 4, 6, 20, 10, 28, 298, DateTimeKind.Unspecified).AddTicks(352), new TimeSpan(0, 7, 0, 0, 0)));
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "UpdatedDate",
+                table: "FarmerPermission",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(2021, 4, 10, 17, 15, 6, 56, DateTimeKind.Unspecified).AddTicks(5146), new TimeSpan(0, 7, 0, 0, 0)),
+                oldClrType: typeof(DateTimeOffset),
+                oldDefaultValue: new DateTimeOffset(new DateTime(2021, 4, 6, 20, 10, 28, 276, DateTimeKind.Unspecified).AddTicks(9785), new TimeSpan(0, 7, 0, 0, 0)));
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "CreatedDate",
+                table: "FarmerPermission",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(2021, 4, 10, 17, 15, 6, 52, DateTimeKind.Unspecified).AddTicks(2191), new TimeSpan(0, 7, 0, 0, 0)),
+                oldClrType: typeof(DateTimeOffset),
+                oldDefaultValue: new DateTimeOffset(new DateTime(2021, 4, 6, 20, 10, 28, 270, DateTimeKind.Unspecified).AddTicks(5697), new TimeSpan(0, 7, 0, 0, 0)));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MilkCollectionStation_District_DistrictId",
+                table: "MilkCollectionStation",
+                column: "DistrictId",
+                principalTable: "District",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MilkCollectionStation_District_DistrictId",
+                table: "MilkCollectionStation");
+
+            migrationBuilder.RenameColumn(
+                name: "DistrictId",
+                table: "MilkCollectionStation",
+                newName: "WardId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_MilkCollectionStation_DistrictId",
+                table: "MilkCollectionStation",
+                newName: "IX_MilkCollectionStation_WardId");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "UpdatedDate",
+                table: "PermissionMembership",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(2021, 4, 6, 20, 10, 28, 303, DateTimeKind.Unspecified).AddTicks(2110), new TimeSpan(0, 7, 0, 0, 0)),
+                oldClrType: typeof(DateTimeOffset),
+                oldDefaultValue: new DateTimeOffset(new DateTime(2021, 4, 10, 17, 15, 6, 72, DateTimeKind.Unspecified).AddTicks(3584), new TimeSpan(0, 7, 0, 0, 0)));
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "CreatedDate",
+                table: "PermissionMembership",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(2021, 4, 6, 20, 10, 28, 303, DateTimeKind.Unspecified).AddTicks(1637), new TimeSpan(0, 7, 0, 0, 0)),
+                oldClrType: typeof(DateTimeOffset),
+                oldDefaultValue: new DateTimeOffset(new DateTime(2021, 4, 10, 17, 15, 6, 72, DateTimeKind.Unspecified).AddTicks(3241), new TimeSpan(0, 7, 0, 0, 0)));
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "UpdatedDate",
+                table: "PermissionGroup",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(2021, 4, 6, 20, 10, 28, 299, DateTimeKind.Unspecified).AddTicks(563), new TimeSpan(0, 7, 0, 0, 0)),
+                oldClrType: typeof(DateTimeOffset),
+                oldDefaultValue: new DateTimeOffset(new DateTime(2021, 4, 10, 17, 15, 6, 69, DateTimeKind.Unspecified).AddTicks(211), new TimeSpan(0, 7, 0, 0, 0)));
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "CreatedDate",
+                table: "PermissionGroup",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(2021, 4, 6, 20, 10, 28, 299, DateTimeKind.Unspecified).AddTicks(121), new TimeSpan(0, 7, 0, 0, 0)),
+                oldClrType: typeof(DateTimeOffset),
+                oldDefaultValue: new DateTimeOffset(new DateTime(2021, 4, 10, 17, 15, 6, 68, DateTimeKind.Unspecified).AddTicks(9874), new TimeSpan(0, 7, 0, 0, 0)));
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "UpdatedDate",
+                table: "MenuPermission",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(2021, 4, 6, 20, 10, 28, 298, DateTimeKind.Unspecified).AddTicks(853), new TimeSpan(0, 7, 0, 0, 0)),
+                oldClrType: typeof(DateTimeOffset),
+                oldDefaultValue: new DateTimeOffset(new DateTime(2021, 4, 10, 17, 15, 6, 67, DateTimeKind.Unspecified).AddTicks(7249), new TimeSpan(0, 7, 0, 0, 0)));
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "CreatedDate",
+                table: "MenuPermission",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(2021, 4, 6, 20, 10, 28, 298, DateTimeKind.Unspecified).AddTicks(352), new TimeSpan(0, 7, 0, 0, 0)),
+                oldClrType: typeof(DateTimeOffset),
+                oldDefaultValue: new DateTimeOffset(new DateTime(2021, 4, 10, 17, 15, 6, 67, DateTimeKind.Unspecified).AddTicks(6882), new TimeSpan(0, 7, 0, 0, 0)));
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "UpdatedDate",
+                table: "FarmerPermission",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(2021, 4, 6, 20, 10, 28, 276, DateTimeKind.Unspecified).AddTicks(9785), new TimeSpan(0, 7, 0, 0, 0)),
+                oldClrType: typeof(DateTimeOffset),
+                oldDefaultValue: new DateTimeOffset(new DateTime(2021, 4, 10, 17, 15, 6, 56, DateTimeKind.Unspecified).AddTicks(5146), new TimeSpan(0, 7, 0, 0, 0)));
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "CreatedDate",
+                table: "FarmerPermission",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(2021, 4, 6, 20, 10, 28, 270, DateTimeKind.Unspecified).AddTicks(5697), new TimeSpan(0, 7, 0, 0, 0)),
+                oldClrType: typeof(DateTimeOffset),
+                oldDefaultValue: new DateTimeOffset(new DateTime(2021, 4, 10, 17, 15, 6, 52, DateTimeKind.Unspecified).AddTicks(2191), new TimeSpan(0, 7, 0, 0, 0)));
+
+            migrationBuilder.CreateTable(
+                name: "Ward",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Code = table.Column<string>(nullable: true),
+                    CreatedByUserId = table.Column<int>(nullable: false),
+                    CreatedByUserName = table.Column<string>(nullable: true),
+                    CreatedDate = table.Column<DateTimeOffset>(nullable: false),
+                    DistrictId = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    RowVersion = table.Column<byte[]>(nullable: true),
+                    Type = table.Column<string>(nullable: true),
+                    UpdatedByUserId = table.Column<int>(nullable: false),
+                    UpdatedByUserName = table.Column<string>(nullable: true),
+                    UpdatedDate = table.Column<DateTimeOffset>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ward", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Ward_District_DistrictId",
+                        column: x => x.DistrictId,
+                        principalTable: "District",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ward_DistrictId",
+                table: "Ward",
+                column: "DistrictId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MilkCollectionStation_Ward_WardId",
+                table: "MilkCollectionStation",
+                column: "WardId",
+                principalTable: "Ward",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
