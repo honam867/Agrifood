73e0bb46175cefd42aeec33ceffee987c5dd02dd
JayThien <80061797+JayThien@users.noreply.github.com>
2021-04-18 01:54:59 +0700

    create storagetank

---
+++ Domains/ApplicationDomain/BOA/IRepositories/IStorageTankRepository.cs
@@ -0,0 +1,18 @@
+﻿using ApplicationDomain.BOA.Entities;
+using AspNetCore.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationDomain.BOA.IRepositories
+{
+    public interface IStorageTankRepository : IGenericRepository<StorageTank, int>
+    {
+        IQueryable GetStorageTanks();
+        IQueryable GetStorageTankById(int id);
+        //Task<bool> CheckCodeExistsAsync(string code);
+        //IQueryable GetStorageTankByDate(int date, int month, int year, int session);
+    }
+}
---
+++ Domains/ApplicationDomain/BOA/Models/StorageTanks/StorageTankModel.cs
@@ -0,0 +1,25 @@
+﻿using ApplicationDomain.BOA.Entities;
+using AutoMapper;
+using System;
+
+namespace ApplicationDomain.BOA.Models.StorageTanks
+{
+    public class StorageTankModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string TypeMilk { get; set; }
+        public int Quantity { get; set; }
+        public int MilkCollectionStationId { get; set; }
+
+    }
+    public class StorageTankModelMapper : Profile
+    {
+        public StorageTankModelMapper()
+        {
+            CreateMap<StorageTankModel, StorageTank>();
+            var mapers = CreateMap<StorageTank, StorageTankModel>();
+        }
+    }
+}
