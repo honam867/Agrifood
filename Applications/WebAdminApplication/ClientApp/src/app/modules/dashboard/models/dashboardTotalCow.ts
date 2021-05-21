ed727f5619916fc650ced47434a69807e3ce61d3
X161026 - NGUYEN THANH NAM - K22X01 <x161026@vanlanguni.vn>
2021-05-21 12:51:21 +0700

    creating chart

---
+++ Applications/WebAdminApplication/ClientApp/src/app/modules/dashboard/models/dashboardTotalCow.ts
@@ -0,0 +1,13 @@
+export class DashBoardTotalCow {
+  tongBo: number;
+  sinhSan: number;
+  boThit: number;
+  be: number;
+  constructor() {
+    this.tongBo = 0;
+    this.sinhSan = 0;
+    this.boThit = 0;
+    this.be = 0;
+
+  }
+}
--- Applications/WebAdminApplication/ClientApp/src/app/modules/dashboard/pages/dashboard-list/dashboard-list.component.ts
+++ Applications/WebAdminApplication/ClientApp/src/app/modules/dashboard/pages/dashboard-list/dashboard-list.component.ts
@@ -1,4 +1,7 @@
+import { DashBoardTotalCow } from './../../models/dashboardTotalCow';
+import { DashBoardService } from './../../dashboard.service';
 import { Component, OnInit } from '@angular/core';
+import {Chart} from "node_modules/chart.js";
 
 @Component({
   selector: 'app-dashboard-list',
@@ -6,10 +9,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard-list.component.scss']
 })
 export class DashboardListComponent implements OnInit {
+  myChart: Chart;
+  totalCow: DashBoardTotalCow = new DashBoardTotalCow();
+  dataTotalCow: any;
 
-  constructor() { }
+
+
+  constructor(
+    public dashBoardService: DashBoardService
+  ) { }
 
   ngOnInit(): void {
+    this.fetchTotalCow();
+    setTimeout(() => {
+      this.showChart();
+    }, 500);
   }
+
+  fetchTotalCow() {
+    this.dashBoardService.getTotalCow().subscribe(
+      res => {
+        this.totalCow = res;
+        this.dataTotalCow = Object.values(this.totalCow[0]);
+        this.dataTotalCow.shift();
+      });
+  }
+
+  showChart(){
+    this.myChart = new Chart("myChart", {
+      type: 'pie',
+      data: {
+        labels: [
+          'Sinh sản',
+          'Bò thịt',
+          'Bê'
+        ],
+        datasets: [{
+          label: 'Tổng bò',
+          data: this.dataTotalCow,
+          backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+            'rgb(255, 205, 86)'
+          ],
+          hoverOffset: 4
+        }]
+      },
+      options: {
+          scales: {
+              y: {
+                  beginAtZero: true
+              }
+          },
+          plugins: {
+
+          }
+      }
+  });
+  }
+
 
 }
