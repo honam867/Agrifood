9a0c2c9ad10777c567e03753fc116c1b8bfb1cdd
JayThien <80061797+JayThien@users.noreply.github.com>
2021-05-23 22:58:56 +0700

    Update Status

---
+++ Infrastructures/Infrastructure.DbMigration/Migrations/20210523155633_updateStatus.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.DbMigration.Migrations
+{
+    public partial class updateStatus : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "UpdatedDate",
+                table: "PermissionMembership",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(2021, 5, 23, 22, 56, 32, 842, DateTimeKind.Unspecified).AddTicks(5110), new TimeSpan(0, 7, 0, 0, 0)),
+                oldClrType: typeof(DateTimeOffset),
+                oldDefaultValue: new DateTimeOffset(new DateTime(2021, 5, 16, 21, 15, 35, 396, DateTimeKind.Unspecified).AddTicks(3236), new TimeSpan(0, 7, 0, 0, 0)));
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "CreatedDate",
+                table: "PermissionMembership",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(2021, 5, 23, 22, 56, 32, 842, DateTimeKind.Unspecified).AddTicks(4750), new TimeSpan(0, 7, 0, 0, 0)),
+                oldClrType: typeof(DateTimeOffset),
+                oldDefaultValue: new DateTimeOffset(new DateTime(2021, 5, 16, 21, 15, 35, 396, DateTimeKind.Unspecified).AddTicks(2867), new TimeSpan(0, 7, 0, 0, 0)));
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "UpdatedDate",
+                table: "PermissionGroup",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(2021, 5, 23, 22, 56, 32, 839, DateTimeKind.Unspecified).AddTicks(841), new TimeSpan(0, 7, 0, 0, 0)),
+                oldClrType: typeof(DateTimeOffset),
+                oldDefaultValue: new DateTimeOffset(new DateTime(2021, 5, 16, 21, 15, 35, 392, DateTimeKind.Unspecified).AddTicks(8473), new TimeSpan(0, 7, 0, 0, 0)));
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "CreatedDate",
+                table: "PermissionGroup",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(2021, 5, 23, 22, 56, 32, 838, DateTimeKind.Unspecified).AddTicks(9981), new TimeSpan(0, 7, 0, 0, 0)),
+                oldClrType: typeof(DateTimeOffset),
+                oldDefaultValue: new DateTimeOffset(new DateTime(2021, 5, 16, 21, 15, 35, 392, DateTimeKind.Unspecified).AddTicks(8058), new TimeSpan(0, 7, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Notify",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "MilkCoupon",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "MilkCollectionStation",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "UpdatedDate",
+                table: "FarmerPermission",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(2021, 5, 23, 22, 56, 32, 834, DateTimeKind.Unspecified).AddTicks(5956), new TimeSpan(0, 7, 0, 0, 0)),
+                oldClrType: typeof(DateTimeOffset),
+                oldDefaultValue: new DateTimeOffset(new DateTime(2021, 5, 16, 21, 15, 35, 380, DateTimeKind.Unspecified).AddTicks(4804), new TimeSpan(0, 7, 0, 0, 0)));
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "CreatedDate",
+                table: "FarmerPermission",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(2021, 5, 23, 22, 56, 32, 824, DateTimeKind.Unspecified).AddTicks(7902), new TimeSpan(0, 7, 0, 0, 0)),
+                oldClrType: typeof(DateTimeOffset),
+                oldDefaultValue: new DateTimeOffset(new DateTime(2021, 5, 16, 21, 15, 35, 371, DateTimeKind.Unspecified).AddTicks(4295), new TimeSpan(0, 7, 0, 0, 0)));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Notify");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "MilkCoupon");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "MilkCollectionStation");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "UpdatedDate",
+                table: "PermissionMembership",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(2021, 5, 16, 21, 15, 35, 396, DateTimeKind.Unspecified).AddTicks(3236), new TimeSpan(0, 7, 0, 0, 0)),
+                oldClrType: typeof(DateTimeOffset),
+                oldDefaultValue: new DateTimeOffset(new DateTime(2021, 5, 23, 22, 56, 32, 842, DateTimeKind.Unspecified).AddTicks(5110), new TimeSpan(0, 7, 0, 0, 0)));
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "CreatedDate",
+                table: "PermissionMembership",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(2021, 5, 16, 21, 15, 35, 396, DateTimeKind.Unspecified).AddTicks(2867), new TimeSpan(0, 7, 0, 0, 0)),
+                oldClrType: typeof(DateTimeOffset),
+                oldDefaultValue: new DateTimeOffset(new DateTime(2021, 5, 23, 22, 56, 32, 842, DateTimeKind.Unspecified).AddTicks(4750), new TimeSpan(0, 7, 0, 0, 0)));
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "UpdatedDate",
+                table: "PermissionGroup",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(2021, 5, 16, 21, 15, 35, 392, DateTimeKind.Unspecified).AddTicks(8473), new TimeSpan(0, 7, 0, 0, 0)),
+                oldClrType: typeof(DateTimeOffset),
+                oldDefaultValue: new DateTimeOffset(new DateTime(2021, 5, 23, 22, 56, 32, 839, DateTimeKind.Unspecified).AddTicks(841), new TimeSpan(0, 7, 0, 0, 0)));
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "CreatedDate",
+                table: "PermissionGroup",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(2021, 5, 16, 21, 15, 35, 392, DateTimeKind.Unspecified).AddTicks(8058), new TimeSpan(0, 7, 0, 0, 0)),
+                oldClrType: typeof(DateTimeOffset),
+                oldDefaultValue: new DateTimeOffset(new DateTime(2021, 5, 23, 22, 56, 32, 838, DateTimeKind.Unspecified).AddTicks(9981), new TimeSpan(0, 7, 0, 0, 0)));
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "UpdatedDate",
+                table: "FarmerPermission",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(2021, 5, 16, 21, 15, 35, 380, DateTimeKind.Unspecified).AddTicks(4804), new TimeSpan(0, 7, 0, 0, 0)),
+                oldClrType: typeof(DateTimeOffset),
+                oldDefaultValue: new DateTimeOffset(new DateTime(2021, 5, 23, 22, 56, 32, 834, DateTimeKind.Unspecified).AddTicks(5956), new TimeSpan(0, 7, 0, 0, 0)));
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "CreatedDate",
+                table: "FarmerPermission",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(2021, 5, 16, 21, 15, 35, 371, DateTimeKind.Unspecified).AddTicks(4295), new TimeSpan(0, 7, 0, 0, 0)),
+                oldClrType: typeof(DateTimeOffset),
+                oldDefaultValue: new DateTimeOffset(new DateTime(2021, 5, 23, 22, 56, 32, 824, DateTimeKind.Unspecified).AddTicks(7902), new TimeSpan(0, 7, 0, 0, 0)));
+        }
+    }
+}
